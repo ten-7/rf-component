36dcaf68904d5457694f6ab7ba4ce4cdadaad9c4
Jonathan <masteraikeus@gmail.com>
2019-03-30 17:00:40 -0500

    funnnn

--- server/index.js
+++ server/index.js
@@ -49,3 +49,22 @@ app.put('/reviews/update', (req, res) => {
     }
   })
 })
+
+app.post('/secret', (req, res) => {
+  fs.readFile(path.join(__dirname, "../dummydata.json"), (error, data) => {
+    if (error) {
+      console.error(error)
+    } else {
+      data = JSON.parse(data);
+      data.map((review, index) => {
+        db.postReview(review, (error, result) => {
+          if (error) {
+            console.error(error);
+          } else {
+            console.log("success", index);
+          }
+        })
+      })
+    }
+  })
+})
