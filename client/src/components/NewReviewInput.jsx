ef3f0cc6a3147563dda15f5ba3c3de54c876d6df
Jonathan <masteraikeus@gmail.com>
2019-04-08 11:42:07 -0500

    working with Grid

--- client/src/components/NewReviewInput.jsx
+++ client/src/components/NewReviewInput.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import StarRatings from 'react-star-ratings';
+import Grid from '@material-ui/core/Grid';
 
 class NewReviewInput extends React.Component {
   constructor (props) {
@@ -69,22 +70,29 @@ class NewReviewInput extends React.Component {
   render () {
     return (
       <div id="newReviewInput">
-        <input name="username" placeholder="Your Name" onChange={event => this.handleChange(event)}></input>
-        <span> Score: </span>
-        <StarRatings rating={this.state.score} 
-                    starRatedColor="orange" 
-                    starHoverColor="orange"
-                    changeRating={this.handleScoreChange.bind(this)} 
-                    numberOfStars={5} 
-                    name='score'
-                    starDimension="20px"/><br />
-        <textarea rows="5" cols="50" name="body" placeholder="Write a Review" onChange={event => this.handleChange(event)}></textarea><br />
-          <div onClick={event => this.setProCon(event)} id="reliability">Reliability: Pro</div>
-          <div onClick={event => this.setProCon(event)} id="durability">Durability: Pro</div>
-          <div onClick={event => this.setProCon(event)} id="looks">Looks: Pro</div>
-          <div onClick={event => this.setProCon(event)} id="performance">Performance: Pro</div>
-          <div onClick={event => this.setProCon(event)} id="value">Value: Pro</div>
-        <div className="submitBtn btn" onClick={(event) => {this.isValidReview(event, this.state)}}>Submit</div>
+        <Grid container>
+          <Grid>
+          <input name="username" placeholder="Your Name" onChange={event => this.handleChange(event)}></input>
+          </Grid>
+          <Grid>
+          <span> Score: </span>
+          <StarRatings rating={this.state.score} 
+                      starRatedColor="orange" 
+                      starHoverColor="orange"
+                      changeRating={this.handleScoreChange.bind(this)} 
+                      numberOfStars={5} 
+                      name='score'
+                      starDimension="20px"/>
+          </Grid>
+          <br />
+          <textarea rows="5" cols="50" name="body" placeholder="Write a Review" onChange={event => this.handleChange(event)}></textarea><br />
+            <div onClick={event => this.setProCon(event)} id="reliability">Reliability: Pro</div>
+            <div onClick={event => this.setProCon(event)} id="durability">Durability: Pro</div>
+            <div onClick={event => this.setProCon(event)} id="looks">Looks: Pro</div>
+            <div onClick={event => this.setProCon(event)} id="performance">Performance: Pro</div>
+            <div onClick={event => this.setProCon(event)} id="value">Value: Pro</div>
+          <div className="submitBtn btn" onClick={(event) => {this.isValidReview(event, this.state)}}>Submit</div>
+        </Grid>
         <hr className="component-div" />
       </div>
     )
