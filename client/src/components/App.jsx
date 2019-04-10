9994f1f64acc6260b803533da6561fefdfcc7a77
Jonathan <masteraikeus@gmail.com>
2019-04-10 12:05:34 -0500

    more event listener debugging

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -16,7 +16,7 @@ class App extends React.Component {
 
   componentDidMount() {
     window.addEventListener('productId', (e) => {
-      console.log("event listener here");
+      console.log("event listener here", e.detail);
       this.setState({
         productId: e.detail
       })
