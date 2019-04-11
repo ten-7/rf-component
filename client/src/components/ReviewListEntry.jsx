dfeca9ebea975bc314c3addbc37d91a572e17c8e
Jonathan <masteraikeus@gmail.com>
2019-04-11 15:45:30 -0500

    environement maybe

--- client/src/components/ReviewListEntry.jsx
+++ client/src/components/ReviewListEntry.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import StarRatings from 'react-star-ratings';
+import { ThumbDown } from '@material-ui/icons/';
+import Typography from '@material-ui/core/Typography';
 
 const ReviewListEntry = (props) => {
   let id = `entry${props.index}`
-
   return (
     <div id={id} className="entry">
       <div>
@@ -11,7 +12,8 @@ const ReviewListEntry = (props) => {
         <span> | <StarRatings 
                     rating={props.review.score} 
                     starRatedColor="orange"
-                    starDimension="20px"/></span>
+                    starDimension="20px"/>
+        </span>
       </div>
         <br />
       <div className="body">
@@ -19,6 +21,7 @@ const ReviewListEntry = (props) => {
       </div>
         <br />
       <div>
+        {/* <ThumbDown /> */}
         <span className="likes" onClick={(event) => { props.changeLikeDislike(event, props.index, "likes") }}>Likes: {props.review.likes}</span>
         <span className="dislikes" onClick={(event) => { props.changeLikeDislike(event, props.index, "dislikes") }}> | Dislikes: {props.review.dislikes}</span>
       </div>
