08c1a23cde00d932afd3e2ed9437eb9ad1912955
Jonathan <masteraikeus@gmail.com>
2019-04-11 09:25:53 -0500

    css changes

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -107,9 +107,6 @@ class App extends React.Component {
           direction='column'
           spacing={8}
           alightItems='center'
-          style={{
-            textAlign: "center"
-          }}
           >
           <NewReviewInput postReview={this.postReview.bind(this)}/>
           <ReviewList reviews={this.state.reviews} changeLikeDislike={this.incrementLikesAndDislikes.bind(this)}/>
