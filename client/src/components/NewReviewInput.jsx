1fa6234738f1d0c3a5cae22d6be87f85177b84da
Jonathan <masteraikeus@gmail.com>
2019-04-15 01:30:39 -0500

    css changes

--- client/src/components/NewReviewInput.jsx
+++ client/src/components/NewReviewInput.jsx
@@ -55,7 +55,7 @@ class NewReviewInput extends React.Component {
       alert("Please fully complete your review!");
     } else {
       this.props.postReview(newReview, () => {
-        
+
       });
     }
   }
@@ -66,17 +66,14 @@ class NewReviewInput extends React.Component {
     let newProsCons = this.state.proscons;
     newProsCons[event.target.id] = !newProsCons[event.target.id];
 
-    let newString = event.target.id;
-    newString = newString.charAt(0).toUpperCase() + newString.slice(1);
-
     this.setState({ 
       proscons: newProsCons
     });
 
     if(newProsCons[event.target.id]) {
-      event.target.innerHTML = `${newString}: Pro`;
+      event.target.innerHTML = `Pro`;
     } else {
-      event.target.innerHTML = `${newString}: Con`;
+      event.target.innerHTML = `Con`;
     }
   }
 
@@ -87,7 +84,8 @@ class NewReviewInput extends React.Component {
         <Grid container spacing={16}>
           <Grid item xs={6}>
             <Paper style={{height: '136px'}}>
-            <br />
+              <div className="input-div">
+              <br />
               <input name="username" placeholder="Your Name" onChange={event => this.handleChange(event)}></input>
               <span> Score: </span>
               <StarRatings rating={this.state.score} 
@@ -98,17 +96,25 @@ class NewReviewInput extends React.Component {
                           name='score'
                           starDimension="20px"/>
               <br />
-              <textarea style={{width: "38.5vw", height: "85px", resize: "none"}} name="body" placeholder="Write a Review" onChange={event => this.handleChange(event)}></textarea>
+              <textarea style={{width: "35.5vw", height: "85px", resize: "none"}} name="body" placeholder="Write a Review" onChange={event => this.handleChange(event)}></textarea>
+              </div>
             </Paper>
           </Grid>
           <Grid item xs={6}>
-            <Paper style={{height: '136px'}}>
+            <Paper style={{height: '136px', alignSelf: "center"}}>
+            <div className="pro-con-div">
               <br/>
-              <span title="Toggle Pro/Con" className="btn pro-con" onClick={event => this.setProCon(event)} id="reliability">Reliability: Pro</span><br />
-              <span title="Toggle Pro/Con" className="btn pro-con" onClick={event => this.setProCon(event)} id="durability">Durability: Pro</span><br />
-              <span title="Toggle Pro/Con" className="btn pro-con" onClick={event => this.setProCon(event)} id="looks">Looks: Pro</span><br />
-              <span title="Toggle Pro/Con" className="btn pro-con" onClick={event => this.setProCon(event)} id="performance">Performance: Pro</span><br />
-              <span title="Toggle Pro/Con" className="btn pro-con" onClick={event => this.setProCon(event)} id="value">Value: Pro</span><br />
+              <span title="Toggle Pro/Con" className="pro-con" >Reliability:
+              <Paper className="btn pro-con-paper" onClick={event => this.setProCon(event)} id="reliability">Pro</Paper></span><br />
+              <span title="Toggle Pro/Con" className="pro-con">Durability:
+              <Paper className="btn pro-con-paper" onClick={event => this.setProCon(event)} id="durability">Pro</Paper></span><br />
+              <span title="Toggle Pro/Con" className="pro-con">Looks:
+              <Paper className="btn pro-con-paper" onClick={event => this.setProCon(event)} id="looks">Pro</Paper></span><br />
+              <span title="Toggle Pro/Con" className="pro-con">Performance:
+              <Paper className="btn pro-con-paper" onClick={event => this.setProCon(event)} id="performance">Pro</Paper></span><br />
+              <span title="Toggle Pro/Con" className="pro-con">Value:
+              <Paper className="btn pro-con-paper" onClick={event => this.setProCon(event)} id="value">Pro</Paper></span><br />
+            </div>
             </Paper>
           </Grid>
         </Grid>
